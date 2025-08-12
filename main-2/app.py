577986998282c1a6864569135f0f32436676bf8c
이현민 <dlgusals0120@c4r1s7.codyssey.kr>
2025-08-12 11:08:18 +0900

    오류 수정사항검색

--- main-2/app.py
+++ main-2/app.py
@@ -2,13 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def Welcome_Bandalgom_Caffee():
-    return render_template("bandalgom.html")
-
 @app.route("/menu")
 def menu():
     return render_template("menu.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
